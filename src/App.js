804ad9b61e589aff941cda4cb810b21c058588a8
tirta71 <tirta4132@gmail.com>
2023-06-02 18:09:37 +0700

    test2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Menu from "./component/menu";
-import NavbarKu from "./component/Navbar";
+
 import ButtonReload from "./component/button/buttonReload";
 
 function App() {
   return (
     <div>
-      <NavbarKu />
       <Menu />
       <ButtonReload />
     </div>
