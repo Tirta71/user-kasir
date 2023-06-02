44f0ff7560fdb4f95deaa9de23ebb0e81df21ee4
tirta71 <tirta4132@gmail.com>
2023-06-02 18:22:57 +0700

    anjay

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Menu from "./component/menu";
-
+import NavbarKu from "./component/Navbar";
 import ButtonReload from "./component/button/buttonReload";
 
 function App() {
   return (
     <div>
+      <NavbarKu />
       <Menu />
       <ButtonReload />
     </div>
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,27 +1,28 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import CartDropdown from "./CartDropdown";
-import { useMediaQuery } from "react-responsive";
 
 export default function NavbarKu() {
-  const isDesktop = useMediaQuery({ minWidth: 992 });
+  const isDesktop = useMediaQuery({ minDeviceWidth: 992 });
 
   return (
     <div>
-      <Navbar variant="dark" bg="dark" expand="lg">
-        <Container fluid>
-          <div className="d-flex align-items-center">
-            <Navbar.Brand href="/">Warkop Ku</Navbar.Brand>
-            {isDesktop && (
-              <Nav className="ms-auto">
+      {isDesktop && (
+        <Navbar variant="dark" bg="dark" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#home">Warkop Ku</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-dark-example" />
+            <Navbar.Collapse id="navbar-dark-example">
+              <Nav>
                 <CartDropdown />
               </Nav>
-            )}
-          </div>
-        </Container>
-      </Navbar>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      )}
     </div>
   );
 }
